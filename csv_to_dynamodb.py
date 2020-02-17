0205b527d5ea33fc9610ff7e170892f6b8e15b62
Anthony Pasquariello <antpas@bu.edu>
2020-02-17 14:28:50 -0500

    Update csv_to_dynamodb.py

--- csv_to_dynamodb.py
+++ csv_to_dynamodb.py
@@ -18,14 +18,36 @@ def lambda_handler(event, context):
     #get() does not store in memory
     obj = s3.Object(bucket, key).get()['Body']
     table = dynamodb.Table(tableName)
-    
+
+    batch_size = 100
+    batch = []
+    count = 0
+
     #DictReader is a generator; not stored in memory
     for row in csv.DictReader(codecs.getreader('utf-8')(obj)):
-        table.put_item(
-           Item=row
-        )
+        if count >= batch_size:
+            write_to_dynamo(batch)
+            batch = []
+            count = 0
+
+        batch.append(row)
+        count += 1
+    
+    if batch:
+        write_to_dynamo(batch)
  
     return {
         'statusCode': 200,
         'body': json.dumps('Uploaded to DynamoDB Table')
     }
+
+    
+def write_to_dynamo(row):
+    table = dynamodb.Table(tableName)
+
+    with table.batch_writer() as batch:
+        for i in range(len(row)):
+            batch.put_item(
+                Item=row[i]
+            )
+    
